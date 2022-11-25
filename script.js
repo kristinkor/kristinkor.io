4455f9ea66d88da4d5add421653a9801020b2c9d
Kristina <kristinkor@gmail.com>
2022-11-25 15:51:18 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ applyInitialTheme();
 window.onload=function(){
   document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
 }
+
+jQuery(document).ready(function($){
+  $('.toggle').on('click', function(){
+    var targetBox = $(this).attr('target-box'); // Find the target box
+
+    $('section').not(targetBox).hide(); // Hide all other boxes
+
+    $(targetBox).show(100); // Toggle the current state of this one
+  });
+});
